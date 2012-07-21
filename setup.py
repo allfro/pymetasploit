b9c0fb7ba8d4c2c75e55c1b8d68fa2b3aa52e79b
Nadeem Douba <ndouba@gmail.com>
2012-07-21 17:01:18 -0400

    First Release

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from os import path
+
+def read(fname):
+    return open(path.join(path.dirname(__file__), fname)).read()
+
+setup(
+    name='pymetasploit',
+    author='Nadeem Douba',
+    version='1.0',
+    author_email='ndouba@gmail.com',
+    description='A full-fledged msfrpc library for Metasploit framework.',
+    license='GPL',
+    packages=find_packages('src'),
+    package_dir={ '' : 'src' },
+    install_requires=[
+        'msgpack-python>=0.1.12'
+    ],
+    url='https://github.com/allfro/pymetasploit',
+    download_url='https://github.com/allfro/pymetasploit/zipball/master',
+    long_description=read('README.md')
+)
